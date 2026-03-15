68ca5ab23e068c41ed7dfeae45ef9e66491ba0d3
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-03-15 12:33:35 -0500

    modify prinicpals passing in iam module

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["${var.iam_principal}"]
+      identifiers = [var.iam_principal]
     }
 
     actions = ["sts:AssumeRole"]
