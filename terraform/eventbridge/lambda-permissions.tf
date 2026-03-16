95ed562f1ee7ca250c0c0a6a2c5ba2c0718bb18d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-03-16 03:14:34 -0500

    fix: remove unused data variables which are unnecessary in eventbridge module

--- terraform/eventbridge/lambda-permissions.tf
+++ terraform/eventbridge/lambda-permissions.tf
@@ -1,7 +1,3 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-data "aws_partition" "current" {}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_permission
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowExecutionFromEventBridge"
