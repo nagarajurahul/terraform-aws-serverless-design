7f48c30ea4ac9ce3ff8bee325b9f4971a72a8805
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-03-19 12:15:20 -0500

    add policy to give permission for process-payment lambda to put to event bus and also create update-inventory lambda with policies

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -50,6 +50,51 @@ locals {
             resources = ["arn:aws:logs:*:*:*"]
           }]
         }
+
+        process-payment-event-bus-policy = {
+          statements = [{
+            effect    = "Allow"
+            actions   = ["events:PutEvents"]
+            resources = [module.eventbridge.event_bus_arn]
+          }]
+        }
+
+      }
+
+    }
+
+    update-inventory-role = {
+      principal = "lambda.amazonaws.com"
+
+      iam_policies = {
+        update-inventory-cloudwatch-logs-policy = {
+          statements = [{
+            effect = "Allow"
+            actions = [
+              "logs:CreateLogGroup",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+            ]
+            resources = ["arn:aws:logs:*:*:*"]
+          }]
+        }
+
+        update-shipment-event-bus-policy = {
+          statements = [{
+            effect    = "Allow"
+            actions   = ["events:PutEvents"]
+            resources = [module.eventbridge.event_bus_arn]
+          }]
+        }
+
+        # update-inventory-dynamodb-policy = {
+        #   statements = [{
+        #     effect    = "Allow"
+        #     actions   = ["dynamodb:PutItem"]
+        #     resources = [module.dynamodb.dynamodb_table_arn]
+        #   }]
+        # }
+
       }
 
     }
