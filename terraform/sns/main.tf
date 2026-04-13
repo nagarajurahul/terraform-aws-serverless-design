5e67c5270ab49b4d1f8c2a5b6d7ca8e7ce8832ee
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-04-12 21:15:08 -0500

    update: sns subscription protocol to email-json due to unknown frequent deletion

--- terraform/sns/main.tf
+++ terraform/sns/main.tf
@@ -6,8 +6,11 @@ resource "aws_sns_topic" "sns" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic_subscription
+# Reason to switch to email-json protocol
+# https://stackoverflow.com/questions/49933987/aws-sns-subscription-keeps-deleting-the-subscription-itself
+# https://medium.com/@IT_Sammy/amazon-sns-email-subscription-problems-26e385ced9f5
 resource "aws_sns_topic_subscription" "sns_topic" {
   topic_arn = aws_sns_topic.sns.arn
-  protocol  = "email"
+  protocol  = "email-json"
   endpoint  = var.sns_topic_email
 }
