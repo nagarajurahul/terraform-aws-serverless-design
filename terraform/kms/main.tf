bd5d3367d06f6eb386a757ffc1b9f48d4912bd9f
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-04-25 20:51:04 -0500

    add module for key-policy and output from key module for key_id

--- terraform/kms/main.tf
+++ terraform/kms/main.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_kms_key" "kms_key" {
   description              = var.key_description
   key_usage                = var.key_usage
@@ -9,23 +7,4 @@ resource "aws_kms_key" "kms_key" {
   rotation_period_in_days = var.rotation_period_in_days
   deletion_window_in_days = var.deletion_window_in_days
 
-}
-
-resource "aws_kms_key_policy" "kms_key_policy" {
-  key_id = aws_kms_key.kms_key.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "key-default-1"
-    Statement = [
-      {
-        Sid    = "Enable IAM User Permissions"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action   = "kms:*"
-        Resource = "*"
-      }
-    ]
-  })
 }
